90d2e98fe3fb9168f0cf0d9fc5b5698a980c734c
Divy <divyparekh1810@gmail.com>
2022-12-26 00:24:39 +0530

    Project setup is done

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ const PORT = process.env.PORT || 3000;
 
 const path = require("path");
 
+app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname) + "/index.html");
 });
 
 app.get("/about", (req, res) => {
   res.sendFile(path.resolve(__dirname) + "/about.html");
+});
+
+app.get("/download", (req, res) => {
+  res.download(path.resolve(__dirname) + "/about.html");
 });
 
 app.listen(PORT, () => {
